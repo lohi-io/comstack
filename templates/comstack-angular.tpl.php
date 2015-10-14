b407e8b1390c13a2df1348d97e19cc757795ee08
Alli Price <allister.price@gmail.com>
2015-10-14 14:35:44 +0000

    Expand the hook theme for angular template.

--- templates/comstack-angular.tpl.php
+++ templates/comstack-angular.tpl.php
@@ -3,6 +3,6 @@
   <div ng-include src="'<?php print $url; ?>'"></div>
   <?php endif; ?>
   <?php if ($ui_view): ?>
-  <ui-view></ui-view>
+  <ui-view<?php print drupal_attributes($ui_view_attributes); ?>></ui-view>
   <?php endif; ?>
 </div>
